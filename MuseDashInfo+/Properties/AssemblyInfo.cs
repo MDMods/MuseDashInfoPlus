bd51fb1737f4f16873d56e3b6ac6cbfdc378af24
KARPED1EM <leever.zzz@gmail.com>
2025-01-13 00:57:39 +0800

    fix: repo link in Assemblyinfo.cs

--- MuseDashInfo+/Properties/AssemblyInfo.cs
+++ MuseDashInfo+/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using MuseDashInfoPlus;
 
-[assembly: MelonInfo(typeof(InfoPlusMod), "Info Plus", "1.0.0", "KARPED1EM", "https://github.com/KARPED1EM/MuseDashInfo+")]
+[assembly: MelonInfo(typeof(InfoPlusMod), "Info Plus", "1.0.0", "KARPED1EM", "https://github.com/KARPED1EM/MuseDashInfoPlus")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 255, 79, 113)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
