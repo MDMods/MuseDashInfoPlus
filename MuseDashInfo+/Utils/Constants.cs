d4485cf73a48d4249f0b5e2eb3fbe9d4da19c02c
KARPED1EM <leever.zzz@gmail.com>
2025-01-13 19:07:01 +0800

    chore: add Constants.cs

---
+++ MuseDashInfo+/Utils/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MuseDashInfoPlus.Utils;
+
+public static class Constants
+{
+    public const float COUNTS_SINGLE_SIZE = 40f;
+    public const float COUNTS_PRIMARY_SIZE = 30f;
+    public const float COUNTS_SECONDARY_SIZE = 28f;
+
+    public const float SONG_NAME_SIZE = 40f;
+    public const float SONG_DIFFICULTY_SIZE = 27f;
+}
