f594e30e6fbfc776a4dfb186437ce21806f70fad
KARPED1EM <leever.zzz@gmail.com>
2025-01-13 21:27:07 +0800

    chore: adjust COUNTS_SINGLE_SIZE

--- MuseDashInfo+/Utils/Constants.cs
+++ MuseDashInfo+/Utils/Constants.cs
@@ -9,7 +9,7 @@ public static class Constants
 
     public static Vector3 SONG_INFOS_POS = new(777, 520, 0);
 
-    public const int COUNTS_SINGLE_SIZE = 40;
+    public const int COUNTS_SINGLE_SIZE = 38;
     public const int COUNTS_PRIMARY_SIZE = 30;
     public const int COUNTS_SECONDARY_SIZE = 28;
 
