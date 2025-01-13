9af5b952f1a888e450b568e6429f7cf022db344a
KARPED1EM <leever.zzz@gmail.com>
2025-01-14 07:41:18 +0800

    feat: fix vanilla bug which hit lower note in a double will cause an extra miss

---
+++ MuseDashInfo+/Patches/PnlVictoryPatch.cs
@@ -0,0 +1,34 @@
+﻿using Il2Cpp;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace MuseDashInfoPlus.Patches;
+
+[HarmonyPatch(typeof(PnlVictory), nameof(PnlVictory.SetDetailInfo))]
+public class PnlVictorySetDetailInfoPatch
+{
+    private static void Postfix(PnlVictory __instance)
+    {
+        try
+        {
+            var transforms = __instance.pnlVictory.GetComponentsInChildren<Transform>(true);
+
+            foreach (var transform in transforms)
+            {
+                if (transform.gameObject.activeSelf &&
+                    transform.name == "PnlVictory" &&
+                    transform.gameObject != __instance.pnlVictory)
+                {
+                    transform.Find("PnlVictory_3D/DetailInfo/Other/TxtMiss/TxtValue")
+                        .gameObject.GetComponent<UnityEngine.UI.Text>()
+                        .text = Utils.GameStatsUtils.MissCount.ToString();
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Melon<InfoPlusMod>.Logger.Error(e.ToString());
+        }
+    }
+}
