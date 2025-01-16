4b9e4cfa6475aa900dc51d906ef3b4fbbd851960
KARPED1EM <leever.zzz@gmail.com>
2025-01-17 01:50:12 +0800

    fix: repo link

--- MuseDashInfo+/MuseDashInfo+.cs
+++ MuseDashInfo+/MuseDashInfo+.cs
@@ -14,7 +14,7 @@ internal static class ModBuildInfo
     public const string DESCRIPTION = "Displays additional in-game infos";
     public const string AUTHOR = "KARPED1EM";
     public const string VERSION = "1.2.0";
-    public const string REPO_LINK = "https://github.com/KARPED1EM/MDIP";
+    public const string REPO_LINK = "https://github.com/KARPED1EM/MuseDashInfoPlus";
 }
 
 /// <summary>
