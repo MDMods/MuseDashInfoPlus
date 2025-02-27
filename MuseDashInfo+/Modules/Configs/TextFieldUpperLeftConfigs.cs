d368319a0b648f73a8675fccd9f6cd69146e1edd
KARPED1EM <leever.zzz@gmail.com>
2025-02-28 01:00:18 +0800

    chore: change default configs

--- MuseDashInfo+/Modules/Configs/TextFieldUpperLeftConfigs.cs
+++ MuseDashInfo+/Modules/Configs/TextFieldUpperLeftConfigs.cs
@@ -4,12 +4,12 @@ namespace MDIP.Modules.Configs;
 
 public class TextFieldUpperLeftConfigs : ConfigBase, ITextConfig
 {
-    public bool Enabled { get; set; } = false;
-    public string Text { get; set; } = "<b>{acc}</b> <size=40>{accGap}</size>";
-    public float OffsetX { get; set; } = 0;
-    public float OffsetY { get; set; } = 0;
+    public bool Enabled { get; set; } = true;
+    public string Text { get; set; } = "<b>{overview}</b>";
+    public float OffsetX { get; set; } = 20;
+    public float OffsetY { get; set; } = 30;
     public string Font { get; set; } = "Snaps Taste";
-    public int FontSize { get; set; } = 80;
+    public int FontSize { get; set; } = 60;
     public string FontColor { get; set; } = Constants.COLOR_WHITE;
     public bool FontOutlineEnabled { get; set; } = false;
     public string FontOutlineColor { get; set; } = Constants.COLOR_WHITE;
