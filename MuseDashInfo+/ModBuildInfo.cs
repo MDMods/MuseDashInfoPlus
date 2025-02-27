62a8235c29810a1bb59c9f2a604ac5551d93c137
KARPED1EM <leever.zzz@gmail.com>
2025-02-28 00:36:46 +0800

    ver: up to 2.3.1

--- MuseDashInfo+/ModBuildInfo.cs
+++ MuseDashInfo+/ModBuildInfo.cs
@@ -5,6 +5,6 @@ public static class ModBuildInfo
     public const string Name = "Info+";
     public const string Description = "Displays additional in-game infos";
     public const string Author = "KARPED1EM";
-    public const string Version = "2.3.0";
+    public const string Version = "2.3.1";
     public const string RepoLink = "https://github.com/MDMods/MuseDashInfoPlus";
 }
